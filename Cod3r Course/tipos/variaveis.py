319b815d4ee5ab9ba306b2fe5783280d29bddd5f
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-14 01:30:46 -0300

    initial commit

---
+++ Cod3r Course/tipos/variaveis.py
@@ -0,0 +1,14 @@
+a = 3
+
+b = 4 
+
+
+texto = ' sua idade é ...'
+idade = 23 
+print(f'{texto} {idade}')
+
+print( 3 * 'bom dia')
+
+raio = float(input('informe o raio da circunferencia ? '))
+
+print(3.14*raio)
