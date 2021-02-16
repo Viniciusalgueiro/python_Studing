599a5781197843f78382bae08c53369446219735
Viniciusalgueiro <viniciussalgueiro09@gmail.com>
2021-02-16 00:18:33 -0300

    adding Tuplas

--- Cod3r Course/tipos/listas.py
+++ Cod3r Course/tipos/listas.py
@@ -2,8 +2,11 @@ nums = [1, 2, 3]
 
 nums.append(3)
 nums.append(3)
-nums[3] = 100
-
+nums[3] = 100 #Altera um elemento 
+nums.insert(0, -200) #insere e desloca 
 print(nums)
+
+print(nums[5])
+print(nums[-1])
 
 print(len(nums))
---
+++ Cod3r Course/tipos/tuplas.py
@@ -0,0 +1,9 @@
+nomes = ('Ana', 'Bia', 'Gui', 'Julia')
+
+print('Bia' in nomes)
+print (nomes[1:3]) # pega o primeiro e o segundo elemento sem incluir o terceiro
+print (nomes[:-1])
+
+print(len(nomes))
+print(type(nomes))
+print(nomes)
